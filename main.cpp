4124e1541bfa7008737600a3eb9fd066b9aa0f93
Ivo Filot [office.linuxbox] <ivo@zuidstijl.nl>
2012-10-26 12:36:41 +0200

    Adjusted main for H2 molecule

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include <iomanip>
 #include "atom.h"
 #include "cgf.h"
 #include "factorial.h"
@@ -9,11 +10,33 @@
 
 int main() {
 
-	Atom h(1,0,0,0);
+	/* define atoms */
+	Atom h1(1,0,0,0);
+	Atom h2(1,1.4,0,0);
+
+	/* define wavefunctions */
 	CGF psi1("1s",1.24,Vector3(0,0,0));
 	CGF psi2("1s",1.24,Vector3(1.4,0,0));
 
-	std::cout << cgf_nuclear(psi1,psi2,h) << std::endl;
+	/* overlap integrals */
+	std::cout << "\t" << "----S----" << std::endl;
+	std::cout << std::setw(10) << std::setprecision(5) << cgf_overlap(psi1,psi1) << "\t" << cgf_overlap(psi1,psi2) << std::endl;
+	std::cout << std::setw(10) << std::setprecision(5) << cgf_overlap(psi2,psi1) << "\t" << cgf_overlap(psi2,psi2) << std::endl;
+
+	/* kinetic integrals */
+	std::cout << "\t"  << "----T----" << std::endl;
+	std::cout << std::setw(10) << std::setprecision(5) << cgf_kinetic(psi1,psi1) << "\t" << cgf_kinetic(psi1,psi2) << std::endl;
+	std::cout << std::setw(10) << std::setprecision(5) << cgf_kinetic(psi2,psi1) << "\t" << cgf_kinetic(psi2,psi2) << std::endl;
+
+	/* nuclear attraction integrals */
+	std::cout << "\t"  << "----V(1)----" << std::endl;
+	std::cout << std::setw(10) << std::setprecision(5) << cgf_nuclear(psi1,psi1,h1) << "\t" << cgf_nuclear(psi1,psi2,h1) << std::endl;
+	std::cout << std::setw(10) << std::setprecision(5) << cgf_nuclear(psi2,psi1,h1) << "\t" << cgf_nuclear(psi2,psi2,h1) << std::endl;
+
+	std::cout << "\t"  << "----V(2)----" << std::endl;
+	std::cout << std::setw(10) << std::setprecision(5) << cgf_nuclear(psi1,psi1,h2) << "\t" << cgf_nuclear(psi1,psi2,h2) << std::endl;
+	std::cout << std::setw(10) << std::setprecision(5) << cgf_nuclear(psi2,psi1,h2) << "\t" << cgf_nuclear(psi2,psi2,h2) << std::endl;
+
 
 	return 0;
 }
