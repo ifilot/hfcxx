c0dbe667fc3a5084df8411189ad026a06375e1c4
Ivo Filot [office.linuxbox] <ivo@zuidstijl.nl>
2012-11-26 11:36:34 +0100

    Transferring basis sets to seperate files

--- cgf.cpp
+++ cgf.cpp
@@ -13,45 +13,7 @@ CGF::CGF(const std::string &typee, const unsigned int &z, const Vector3 &rr) {
 	type = typee;
 
 	/* options for setting up the GTOs */
-
-	/* (1) HYDROGEN */
-	if(type.compare("1s")==0 && z==1) {
-		gtos.push_back(GTO(0.444635,r,0.168856,0,0,0));
-		gtos.push_back(GTO(0.535328,r,0.623913,0,0,0));
-		gtos.push_back(GTO(0.154329,r,3.42525,0,0,0));
-	}
-	/* (2) HELIUM */
-	if(type.compare("1s")==0 && z==2) {
-		gtos.push_back(GTO(0.444635,r,0.168856,0,0,0));
-		gtos.push_back(GTO(0.535328,r,0.623913,0,0,0));
-		gtos.push_back(GTO(0.154329,r,3.42525,0,0,0));
-	}
-	/* (8) OXYGEN */
-	if(type.compare("1s")==0 && z==8) {
-		gtos.push_back(GTO(0.15432897000000001,r,130.70931999999999,0,0,0));
-		gtos.push_back(GTO(0.53532813999999995,r,23.808861,0,0,0));
-		gtos.push_back(GTO(0.44463454000000002,r,6.4436083000000002,0,0,0));
-	}
-	if(type.compare("2s")==0 && z==8) {
-		gtos.push_back(GTO(-0.099967230000000004,r,5.0331513000000001,0,0,0));
-		gtos.push_back(GTO(0.39951282999999999,r,1.1695960999999999,0,0,0));
-		gtos.push_back(GTO(0.70011546999999996,r,0.38038899999999998,0,0,0));
-	}
-	if(type.compare("2px")==0 && z==8) {
-		gtos.push_back(GTO(0.15591627,r,5.0331513000000001,1,0,0));
-		gtos.push_back(GTO(0.60768372000000004,r,1.1695960999999999,1,0,0));
-		gtos.push_back(GTO(0.39195739000000002,r,0.38038899999999998,1,0,0));
-	}
-	if(type.compare("2py")==0 && z==8) {
-		gtos.push_back(GTO(0.15591627,r,5.0331513000000001,0,1,0));
-		gtos.push_back(GTO(0.60768372000000004,r,1.1695960999999999,0,1,0));
-		gtos.push_back(GTO(0.39195739000000002,r,0.38038899999999998,0,1,0));
-	}
-	if(type.compare("2pz")==0 && z==8) {
-		gtos.push_back(GTO(0.15591627,r,5.0331513000000001,0,0,1));
-		gtos.push_back(GTO(0.60768372000000004,r,1.1695960999999999,0,0,1));
-		gtos.push_back(GTO(0.39195739000000002,r,0.38038899999999998,0,0,1));
-	}
+	addGTOs_sto3g(type,z);
 }
 
 std::ostream& operator <<(std::ostream &os,const CGF &rhs) {
@@ -71,3 +33,47 @@ std::ostream& operator <<(std::ostream &os,const CGF &rhs) {
 const std::string CGF::orb() const {
 	return type;
 }
+
+void CGF::addGTO_s(double alpha, double c) {
+	gtos.push_back(GTO(c, r, alpha, 0,0,0));
+}
+
+void CGF::addGTO_px(double alpha, double c) {
+	gtos.push_back(GTO(c, r, alpha, 1,0,0));
+	gtos.push_back(GTO(c, r, alpha, 0,1,0));
+	gtos.push_back(GTO(c, r, alpha, 0,0,1));
+}
+
+void CGF::addGTO_py(double alpha, double c) {
+	gtos.push_back(GTO(c, r, alpha, 0,1,0));
+}
+
+void CGF::addGTO_pz(double alpha, double c) {
+	gtos.push_back(GTO(c, r, alpha, 0,0,1));
+}
+
+void CGF::addGTO_dx2(double alpha, double c) {
+	gtos.push_back(GTO(c, r, alpha, 2,0,0));
+}
+
+void CGF::addGTO_dxy(double alpha, double c) {
+	gtos.push_back(GTO(c, r, alpha, 1,1,0));
+}
+
+void CGF::addGTO_dxz(double alpha, double c) {
+	gtos.push_back(GTO(c, r, alpha, 1,0,1));
+}
+
+void CGF::addGTO_dy2(double alpha, double c) {
+	gtos.push_back(GTO(c, r, alpha, 0,2,0));
+}
+
+void CGF::addGTO_dyz(double alpha, double c) {
+	gtos.push_back(GTO(c, r, alpha, 0,1,1));
+}
+
+void CGF::addGTO_dz2(double alpha, double c) {
+	gtos.push_back(GTO(c, r, alpha, 0,0,2));
+}
+
+#include "basis-sto3g.cpp"
--- cgf.h
+++ cgf.h
@@ -32,6 +32,19 @@ class CGF{
 	const std::string orb() const;
 
 	friend std::ostream& operator <<(std::ostream &os,const CGF &rhs);
+
+	private:
+	void addGTOs_sto3g(std::string type, unsigned int z);
+	void addGTO_s(double alpha, double c);
+	void addGTO_px(double alpha, double c);
+	void addGTO_py(double alpha, double c);
+	void addGTO_pz(double alpha, double c);
+	void addGTO_dx2(double alpha, double c);
+	void addGTO_dxy(double alpha, double c);
+	void addGTO_dxz(double alpha, double c);
+	void addGTO_dy2(double alpha, double c);
+	void addGTO_dyz(double alpha, double c);
+	void addGTO_dz2(double alpha, double c);
 };
 
 #endif //_CGF_H
