e0b8320694d48794b01829225848908bb291f1d1
Ivo Filot <ivo@zuidstijl.nl>
2013-01-05 13:38:17 +0100

    Reversioning - implementing tests

--- tests/vector_test.cpp
+++ tests/vector_test.cpp
@@ -1,5 +1,6 @@
 #include "vector_test.h"
 #include "vector.h"
+#include "func.h"
 
 // Registers the fixture into the 'registry'
 CPPUNIT_TEST_SUITE_REGISTRATION( VectorTest );
@@ -23,10 +24,36 @@ VectorTest::testConstructor()
   const double z = 3.3;
 
   // Process
-  Vector3 vec(x,y,z);
+  Vector3 vec(x,y,z); /* test regular constructor */
+	Vector3 vec2(vec); /* test copy constructor */
 
   // Check
   CPPUNIT_ASSERT_EQUAL( x, vec.getx() );
   CPPUNIT_ASSERT_EQUAL( y, vec.gety() );
   CPPUNIT_ASSERT_EQUAL( z, vec.getz() );
+	CPPUNIT_ASSERT_EQUAL( x, vec2.getx() );
+  CPPUNIT_ASSERT_EQUAL( y, vec2.gety() );
+  CPPUNIT_ASSERT_EQUAL( z, vec2.getz() );
+}
+
+void 
+VectorTest::testDist2Function()
+{
+  // Set up
+  const double x1 = 1.1;
+  const double y1 = 2.2;
+  const double z1 = 3.3;
+
+	const double x2 = 4.4;
+  const double y2 = 5.5;
+  const double z2 = 6.6;
+
+	const double ans = 32.67;
+
+  // Process
+  Vector3 a(x1,y1,z1); /* construct */
+	Vector3 b(x2,y2,z2); /* construct */
+
+  // Check
+  CPPUNIT_ASSERT_EQUAL( ans, dist2(a,b) );
 }
--- tests/vector_test.h
+++ tests/vector_test.h
@@ -7,13 +7,17 @@ class VectorTest : public CppUnit::TestFixture
 {
   CPPUNIT_TEST_SUITE( VectorTest );
   CPPUNIT_TEST( testConstructor );
+	CPPUNIT_TEST( testDist2Function );
   CPPUNIT_TEST_SUITE_END();
+
+
 
 public:
   void setUp();
   void tearDown();
 
   void testConstructor();
+	void testDist2Function();
 };
 
 #endif  // _VECTOR_TEST_H
