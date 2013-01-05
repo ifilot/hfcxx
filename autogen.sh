dc811edfe426c182167c85f0bf8d5a0e57e855c0
Ivo Filot <ivo@zuidstijl.nl>
2013-01-05 11:51:32 +0100

    Restructuring

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,6 @@
-#! /bin/sh
-
-aclocal \
-&& automake --add-missing \
-&& autoconf
+#!/bin/sh
+ echo "Running aclocal..." ; aclocal $ACLOCAL_FLAGS || exit 1
+ echo "Running autoheader..." ; autoheader || exit 1
+ echo "Running autoconf..." ; autoconf || exit 1
+ echo "Running automake..." ; automake --add-missing --copy --gnu || exit 1
+ ./configure "$@"
